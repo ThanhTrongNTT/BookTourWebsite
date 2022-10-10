73c5a086d791844b751bb4482441e253ef5d579f
Nguyen Thanh Trong <tronglagi111222@gmail.com>
2022-10-10 22:25:59 +0700

    update UserRoleController

--- BookTourBackEnd/src/main/java/nhom04/hcmute/controller/UserRoleController.java
+++ BookTourBackEnd/src/main/java/nhom04/hcmute/controller/UserRoleController.java
@@ -1,7 +1,9 @@
 package nhom04.hcmute.controller;
 
+import lombok.RequiredArgsConstructor;
 import nhom04.hcmute.model.Role;
 import nhom04.hcmute.payload.ApiResponse;
+import nhom04.hcmute.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,14 +16,18 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class UserRoleController {
+    private final UserService userService;
 
     @PostMapping("/role/save")
     public ResponseEntity<ApiResponse> saveRole(@RequestBody Role role){
+        Role saveRole = userService.saveRole(role);
         return ResponseEntity.ok().body(new ApiResponse(true,"Add role success!"));
     }
-    @PostMapping("/role/addroletouser/{username}")
-    public ResponseEntity<ApiResponse> addRoleToUser(@PathVariable("username") String username,@RequestBody Role role){
+    @PostMapping("/role/addtouser/{email}")
+    public ResponseEntity<ApiResponse> addRoleToUser(@PathVariable("email") String email,@RequestBody Role role){
+        userService.addRoleToUser(email,role);
         return ResponseEntity.ok().body(new ApiResponse(true,"Add role to user success"));
     }
 }
