fe1da95a45614be9b0dfab4f8cd93aa0a598aa77
HoQuocVinh <89761777+HoQuocVinh@users.noreply.github.com>
2022-11-30 19:12:59 +0700

    Call API Booking

--- BookTourFrontEnd/src/hooks/useGetTour.js
+++ BookTourFrontEnd/src/hooks/useGetTour.js
@@ -5,7 +5,6 @@ export function useGetTourById(id) {
   const [tourDetail, setTourDetail] = useState([]);
   useEffect(() => {
     axios.get(`tour/${id}`).then((response) => {
-      console.log(response.data.tourDetail);
       setTourDetail(response.data.tourDetail);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
